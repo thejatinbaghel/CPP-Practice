f59def652989ee7ef8e14d63d155d0aa939e49e2
Jatin Baghel <baghelj008@gmail.com>
2025-02-17 19:35:54 +0530

    Code updated

--- new.cpp
+++ new.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool rev(string s, int i){
     if(i>=s.size()/2) return true;
-    if(s[i]!=s[s.size()-i-1]) return false;
+    if(s[i]!=s[s.size()-1-i]) return false;
 }
 
 int main(){
--- palindrome.cpp
+++ palindrome.cpp
@@ -4,10 +4,11 @@ using namespace std;
 bool pal(int i, string s){
     if(i>=s.size()/2) return true;
     if(s[i]!=s[s.size()-i-1]) return false;
+    return (i+1);
 }
 
 int main(){
-    string s="malayalam";
+    string s="nama";
     cout<<pal(0, s);
     return 0;
 }
