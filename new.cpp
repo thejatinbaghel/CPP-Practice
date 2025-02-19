6baadf6c2ff6f800ef14e76f0e9683059a2dbe6b
Jatin Baghel <baghelj008@gmail.com>
2025-02-19 10:36:08 +0530

    Update file

--- new.cpp
+++ new.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cnt=0;
 int main(){
     int n;
     cin >> n;
 
     while(n>0){
         cout<<n%10;
+        cnt+=1;
         n=n/10;
     }
+    cout << endl;
+    cout << cnt;
 }
