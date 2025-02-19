e3af416cd418232625fea0e3db1e8bef2ed85fd8
Jatin Baghel <baghelj008@gmail.com>
2025-02-19 10:38:14 +0530

    Nice

--- new.cpp
+++ new.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt=0;
+int rev=0;
 int main(){
     int n;
     cin >> n;
 
     while(n>0){
-        cout<<n%10;
-        cnt+=1;
+        rev = rev*10+n%10;
         n=n/10;
     }
-    cout << endl;
-    cout << cnt;
+    cout << endl;    cout << rev;
 }
