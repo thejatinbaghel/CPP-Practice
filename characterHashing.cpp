3e0eba4490fd96c16afa789616b9836fe802e275
Jatin Baghel <baghelj008@gmail.com>
2025-02-26 00:36:16 +0530

    Practice more character hashing

--- characterHashing.cpp
+++ characterHashing.cpp
@@ -3,17 +3,17 @@ using namespace std;
 int main(){
     string s;
     cin >> s;
-    int hash[26] = {0};
+    int hash[256] = {0};
     for (int i = 0; i < s.size(); i++)
     {
-        hash[s[i]-'a']++;
+        hash[s[i]]++;
     }   
     int q;
     cin >> q;
     while(q--){
         char c;
         cin >> c;
-        cout << hash[c-'a'] << endl;
+        cout << hash[c] << endl;
     }
     
     return 0;
