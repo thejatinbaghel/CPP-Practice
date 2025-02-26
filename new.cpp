aeffff76506e1e4180675e9d1d02a8f029e9bddd
Jatin Baghel <baghelj008@gmail.com>
2025-02-26 21:24:33 +0530

    Practice Map

--- new.cpp
+++ new.cpp
@@ -1,33 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-vector<int> plusOne(vector<int>& digits) {
-    
-    for (int i = digits.size() - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
-        }
-        digits[i] = 0;
-    }
-
-    digits.insert(digits.begin(), 1);
-    return digits;
-}
 int main(){
     int sizeOfArray;
     cin >> sizeOfArray;
-    vector<int> digits(sizeOfArray);
+    int arr[sizeOfArray];
     for (int i = 0; i < sizeOfArray; i++)
     {
-        cin >> digits[i];
+        cin >> arr[i];
     }
-    plusOne(digits);
-    for (int i = 0; i < digits.size(); i++)
+    map<int, int> mp;
+    for (int i = 0; i < sizeOfArray; i++)
     {
-        cout << digits[i];
+        mp[arr[i]]++;
+    }
+    
+    int q;
+    cin >> q;
+    while(q--){
+        int number;
+        cin >> number;
+        cout << mp[number] << endl;
     }
     
     return 0;
 }
-        
