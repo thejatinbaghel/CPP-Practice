3da48103a2cc6c50bd4db5efd00bbb6f15818f3c
Jatin Baghel <baghelj008@gmail.com>
2025-02-26 09:12:01 +0530

    Practice more

--- new.cpp
+++ new.cpp
@@ -1,28 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
+int main(){
+    int num;
+    cin >> num;
+    int arr[num];
+    for (int i = 0; i < num; i++)
+    {
         cin >> arr[i];
     }
-
-    //precompute:
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]] += 1;
+    for (int i = 0; i < num; i++)
+    {
+        cout << arr[i] << endl;
     }
-
-    int q;
-    cin >> q;
-    while (q) {
-        int number;
-        cin >> number;
-        // fetching:
-        cout << hash[number] << endl;
-    }
+    
     return 0;
 }
