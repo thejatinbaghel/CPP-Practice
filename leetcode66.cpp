f027a4bad5fb83e0936a28b4b56408720087c07a
Jatin Baghel <baghelj008@gmail.com>
2025-02-26 20:52:14 +0530

    Solve question

--- leetcode66.cpp
+++ leetcode66.cpp
@@ -1,18 +1,32 @@
-class Solution {
-    public:
-        vector<int> plusOne(vector<int>& digits) {
-            int n = digits.size();
-            
-            for (int i = n - 1; i >= 0; i--) {
-                if (digits[i] < 9) {
-                    digits[i]++;
-                    return digits;
-                }
-                digits[i] = 0;
-            }
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits) {
     
-            digits.insert(digits.begin(), 1);
+    for (int i = digits.size() - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
             return digits;
         }
-    };
+        digits[i] = 0;
+    }
+
+    digits.insert(digits.begin(), 1);
+    return digits;
+}
+int main(){
+    int sizeOfArray;
+    cin >> sizeOfArray;
+    vector<int> digits(sizeOfArray);
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        cin >> digits[i];
+    }
+    plusOne(digits);
+    for (int i = 0; i < digits.size(); i++)
+    {
+        cout << digits[i];
+    }
     
+    return 0;
+}
--- new.cpp
+++ new.cpp
@@ -1,8 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+vector<int> plusOne(vector<int>& digits) {
+    
+    for (int i = digits.size() - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    digits.insert(digits.begin(), 1);
+    return digits;
+}
 int main(){
-    string s = "sdjkf";
-    cout << s[1];
+    int sizeOfArray;
+    cin >> sizeOfArray;
+    vector<int> digits(sizeOfArray);
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        cin >> digits[i];
+    }
+    plusOne(digits);
+    for (int i = 0; i < digits.size(); i++)
+    {
+        cout << digits[i];
+    }
     
     return 0;
 }
+        
