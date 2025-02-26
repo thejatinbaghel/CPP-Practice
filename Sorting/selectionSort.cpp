7168070c0d5ebdc47c15837e70137eda760a9cad
Jatin Baghel <baghelj008@gmail.com>
2025-02-27 00:02:58 +0530

    More practice

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -11,7 +11,7 @@ int main(){
     for (int i = 0; i < n-1; i++)
     {
         int mini = i;
-        for (int j = i; j < n-1; j++)
+        for (int j = i+1; j < n; j++)
         {
             if(arr[j]<arr[mini]) mini = j;
         }
