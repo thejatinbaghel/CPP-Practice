01c9aa7f8411118234fa6c3c9bdb0be8329c9a32
Jatin Baghel <baghelj008@gmail.com>
2025-02-27 21:25:59 +0530

    More practice

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
     int n;
     cin >> n;
-    vector<int> arr(n);
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    for (int i = n - 1; i >= 0; i--)
+    
+    for (int i = 0; i < n-1; i++)
     {
-        int didSwap = 0;
-        for (int j = 0; j <= i - 1; j++)
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (arr[j] > arr[j + 1])
-            {
-                swap(arr[j], arr[j + 1]);
-                didSwap = 1;
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
             }
-        }
-        if (didSwap == 0)
-            break;
+        }      
+        if(!swapped) break;
     }
-
+    
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-
+     
     return 0;
 }
--- jspwTest.cpp
+++ jspwTest.cpp
@@ -11,15 +11,15 @@ int main(){
     
     for (int i = 0; i < n-1; i++)
     {
-        int didSwap = 0;
-        for (int j = 1; j < n; j++)
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j]<arr[j-1]){
-                swap(arr[j], arr[j-1]);
-                didSwap = 1;
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
             }
         }      
-        if(didSwap>0) break;
+        if(!swapped) break;
     }
     
     for (int i = 0; i < n; i++)
